7c3de297e16cc2aa9ec0ab9c19d3b6f5abdfb9e4
Vijayashree Kudupudi <vijayshreerao96@gmail.com>
2020-04-11 12:01:07 +0530

    This application is designed from HTML, CSS, vanilla javascript for booking a seat/seats for watching a movie in a theater. Here the user entered data is stored in local storage and populate the UI based on stored data.

---
+++ script.js
@@ -0,0 +1,112 @@
+const container = document.querySelector('.container');
+const seats = document.querySelectorAll('.row .seat:not(.ocuupied)');
+const count = document.getElementById('count');
+const total = document.getElementById('total');
+const movieSelect = document.getElementById('movie');
+
+populateUI();
+
+let ticketPrice = +movieSelect.value; //+sign before makes string to number type
+// console.log(ticketPrice);
+
+//to clear all selected seats
+const clear = document.querySelector('.circle');
+
+//to update the confirmation of seats
+const payment = document.getElementById('payment');
+console.log(payment);
+
+//Update total and count
+function updateSelectedCount() {
+    const selectedSeats = document.querySelectorAll('.row .seat.selected');
+    
+    // console.log(selectedSeats);
+    const selectedSeatsCounts = selectedSeats.length;
+    count.innerText = selectedSeatsCounts;
+    total.innerText = selectedSeatsCounts * ticketPrice;
+
+    //on reoad everything need to be stored in local storage
+    //1. Copy selected seats intoan array
+    //2. Map through array
+    //3. return a new array of indexes
+
+    const seatsIndex = [...selectedSeats].map(seat => [...seats].indexOf(seat));  //[...something] is a spread operator, it copies/shows values of that variables. Here it converts nodelist into regular array
+    // console.log(seatsIndex);
+
+    //to store in localstorage
+    localStorage.setItem('selectedSeats', JSON.stringify(seatsIndex));
+}
+
+//Save selected mobie index and price
+function setMovieData(movieIndex, moviePrice) {
+    localStorage.setItem('selectedMovieIndex', movieIndex);
+    localStorage.setItem('selectedMoviePrice', moviePrice);
+}
+
+//Get data from localStorage and populate UI
+function populateUI() {
+    const selectedSeats = JSON.parse(localStorage.getItem('selectedSeats'));
+    console.log(selectedSeats);
+
+    if(selectedSeats !== null && selectedSeats.length > 0) {
+        seats.forEach((seat, index) => {
+            if(selectedSeats.indexOf(index) > -1) {
+                seat.classList.add('selected');
+            }
+        });
+    }
+
+    const selectedMovieIndex = localStorage.getItem('selectedMovieIndex');
+
+    if(selectedMovieIndex !== null) {
+        movieSelect.selectedIndex = selectedMovieIndex; //as we are fetching from setMovieData() and here we are setting it
+    }
+}
+
+//Seat click event
+container.addEventListener('click', e => {
+    if(e.target.classList.contains('seat') && 
+    !e.target.classList.contains('occupied')) {
+        e.target.classList.toggle('selected');
+
+        updateSelectedCount();
+    }
+})
+
+//Movie click event
+movieSelect.addEventListener('change', e => {
+    ticketPrice = e.target.value;
+
+    updateSelectedCount();
+
+    //To check which movie we selected and cost of that movie
+    //console.log(e.target.selectedIndex, e.target.value);
+    setMovieData(e.target.selectedIndex, e.target.value);
+})
+
+//On reload count of seats gets relaod and thats bcoz when the updateSelectedCount() is called, so the value inside this function doesnt get updated on reload so call at bottom.
+//Initial count and total set
+updateSelectedCount();
+
+//Clearing all the seats selected
+clear.addEventListener('click', (e) =>{
+    localStorage.clear();
+    seats.forEach(seat => {
+        seat.classList.remove('selected');
+    })
+    count.innerText = 0;
+    total.innerText = 0;
+});
+
+//Converting selected seats to occupied
+payment.addEventListener('click', e => {
+    
+    const selectedSeats = JSON.parse(localStorage.getItem('selectedSeats'));
+    if(selectedSeats !== null && selectedSeats.length > 0) {
+        seats.forEach((seat, index) => {
+            if(selectedSeats.indexOf(index) > -1) {
+                seat.classList.add('occupied');
+            }
+        });
+    }
+})
